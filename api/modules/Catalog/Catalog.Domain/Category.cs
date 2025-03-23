f26a94b73ad60cb0af27086195f1703c9eb90df8
Roel D. Caperig <leor3824@yahoo.com>
2025-03-23 21:36:40 +0800

    Add Category class and domain event records
    Introduced the `Category` class implementing `IAggregateRoot` with properties for `Name` and `Description`, along with methods for creation and updates that trigger domain events. Added `CategoryCreated` and `CategoryUpdated` records to represent category creation and updates in domain events.

---
+++ api/modules/Catalog/Catalog.Domain/Category.cs
@@ -0,0 +1,51 @@
+﻿using AMIS.Framework.Core.Domain;
+using AMIS.Framework.Core.Domain.Contracts;
+using AMIS.WebApi.Catalog.Domain.Events;
+
+namespace AMIS.WebApi.Catalog.Domain;
+public class Category : AuditableEntity, IAggregateRoot
+{
+    public string Name { get; private set; } = string.Empty;
+    public string? Description { get; private set; }
+
+    private Category() { }
+
+    private Category(Guid id, string name, string? description)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        QueueDomainEvent(new CategoryCreated { Category = this });
+    }
+
+    public static Category Create(string name, string? description)
+    {
+        return new Category(Guid.NewGuid(), name, description);
+    }
+
+    public Category Update(string? name, string? description)
+    {
+        bool isUpdated = false;
+
+        if (!string.IsNullOrWhiteSpace(name) && !string.Equals(Name, name, StringComparison.OrdinalIgnoreCase))
+        {
+            Name = name;
+            isUpdated = true;
+        }
+
+        if (!string.Equals(Description, description, StringComparison.OrdinalIgnoreCase))
+        {
+            Description = description;
+            isUpdated = true;
+        }
+
+        if (isUpdated)
+        {
+            QueueDomainEvent(new CategoryUpdated { Category = this });
+        }
+
+        return this;
+    }
+}
+
+
---
+++ api/modules/Catalog/Catalog.Domain/Events/CategoryUpdated.cs
@@ -0,0 +1,7 @@
+﻿using AMIS.Framework.Core.Domain.Events;
+
+namespace AMIS.WebApi.Catalog.Domain.Events;
+public sealed record CategoryUpdated : DomainEvent
+{
+    public Category? Category { get; set; }
+}
