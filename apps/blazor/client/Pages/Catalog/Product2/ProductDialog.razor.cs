71907b187612b9e716799e820b51103abb672ad7
PMO <leor3824@yahoo.com>
2025-04-02 14:41:54 +0800

    Enhance ProductDialog with category selection and image upload
    Updated `ProductDialog.razor` to include a `MudSelect` for category selection, replacing the `MudTextField` for `CategoryId`. Improved layout and added UI elements for image upload, including a preview and options to view or clear the uploaded image.
    
    Changed `Model` type in `ProductDialog.razor.cs` from `UpdateProductCommand?` to `ProductViewModel`. Introduced new methods for handling category changes, initializing category data, and uploading images with validation for file type and size. Added functionality to clear the current image.

--- apps/blazor/client/Pages/Catalog/Product2/ProductDialog.razor.cs
+++ apps/blazor/client/Pages/Catalog/Product2/ProductDialog.razor.cs
@@ -1,7 +1,9 @@
 using AMIS.Blazor.Client.Components;
 using AMIS.Blazor.Infrastructure.Api;
+using AMIS.Shared.Authorization;
 using Mapster;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using MudBlazor;
 
 namespace AMIS.Blazor.Client.Pages.Catalog.Product2;
@@ -12,16 +14,20 @@ public partial class ProductDialog
 
     [CascadingParameter]
     private IMudDialogInstance MudDialog { get; set; } = default!;
-    [Parameter] public UpdateProductCommand? Model { get; set; }
-    [Parameter] public EventCallback OnCancel { get; set; }
-   
+    [Parameter] public ProductViewModel Model { get; set; }
+
     [Parameter] public Action? Refresh { get; set; }
     [Parameter] public bool? IsCreate { get; set; }
     [Inject] private ISnackbar Snackbar { get; set; } = default!;
+
+    private List<CategoryResponse> category { get; set; } = default!;
     private string? _successMessage;
+    private List<CategoryResponse> _categories = [];
 
     private MudForm? _form;
     private bool _uploading;
+    private bool _isUploading;
+    private string _uploadErrorMessage;
     private const long MaxAllowedSize = 3145728;
 
     private async Task OnValidSubmit()
@@ -61,6 +67,86 @@ public partial class ProductDialog
                 Refresh?.Invoke();
             }
         }
+    }
+    private void OnCategoryChanged(List<CategoryResponse> Category)
+    {
+        _categories = Category;
+    }
+    protected override async Task OnInitializedAsync()
+    {
+        if (_categories.Count == 0)
+        {
+            var response = await ProductClient.SearchCategorysEndpointAsync("1", new SearchCategorysCommand());
+            if (response?.Items != null)
+            {
+                _categories = response.Items.ToList();
+            }
+        }
+    }
+
+    private async Task UploadFiles(InputFileChangeEventArgs e)
+    {
+        _uploadErrorMessage = string.Empty;
+
+        if (e.File == null)
+        {
+            Snackbar.Add("No e.File selected.", Severity.Error);
+            return;
+        }
+
+        string? extension = Path.GetExtension(e.File.Name);
+
+        // Check if the e.File has a supported image format
+        if (!AppConstants.SupportedImageFormats.Contains(extension.ToLower()))
+        {
+            Snackbar.Add("Image format not supported.", Severity.Error);
+            return;
+        }
+
+        // e.File size validation (5MB max)
+        if (e.File.Size > AppConstants.MaxAllowedSize)
+        {
+            Snackbar.Add("e.File size exceeds the maximum allowed size.", Severity.Error);
+            return;
+        }
+
+        Model.ImageExtension = extension;
+
+        try
+        {
+            // Show progress indicator while uploading
+            _isUploading = true;
+            string? fileName = $"{Model.Name}-{Guid.NewGuid():N}";
+            fileName = fileName[..Math.Min(fileName.Length, 90)];
+            // Request the image e.File to be resized (if necessary) to fit within the specified max width and height
+            var imageFile = await e.File.RequestImageFileAsync(AppConstants.StandardImageFormat, AppConstants.MaxImageWidth, AppConstants.MaxImageHeight);
+
+            // Read the e.File's bytes into a buffer
+            byte[] buffer = new byte[imageFile.Size];
+            await imageFile.OpenReadStream(AppConstants.MaxAllowedSize).ReadExactlyAsync(buffer);
+            string? base64String = $"data:{AppConstants.StandardImageFormat};base64,{Convert.ToBase64String(buffer)}";
+            // Convert the image bytes to a Base64 string
+            Model.Image = new FileUploadCommand() { Name = fileName, Data = base64String, Extension = extension };
+
+            // Trigger a UI update            
+        }
+        catch (Exception ex)
+        {
+            // Handle any exceptions that occur during the e.File upload process
+            Snackbar.Add($"An error occurred while uploading the image: {ex.Message}", Severity.Error);
+        }
+        finally
+        {
+            // Hide the progress indicator once the upload is complete or fails
+            _isUploading = false;
+        }
+    }
+    public void SetDeleteCurrentImageFlag()
+    {
+        Model.Image = null;
+        Model.ImageInBytes = string.Empty;
+        Model.ImagePath = string.Empty;
+        Model.DeleteCurrentImage = true;
     }
 
     private void Cancel()
