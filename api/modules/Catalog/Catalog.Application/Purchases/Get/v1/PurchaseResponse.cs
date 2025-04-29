49dd3d12e67ba4a16c2993aeadd2454a91fe6492
PMO <leor3824@yahoo.com>
2025-04-29 17:20:03 +0800

    April 29 modifications, products and purchase

--- api/modules/Catalog/Catalog.Application/Purchases/Get/v1/PurchaseResponse.cs
+++ api/modules/Catalog/Catalog.Application/Purchases/Get/v1/PurchaseResponse.cs
@@ -3,7 +3,4 @@ using AMIS.WebApi.Catalog.Application.Suppliers.Get.v1;
 using AMIS.WebApi.Catalog.Domain.ValueObjects;
 
 namespace AMIS.WebApi.Catalog.Application.Purchases.Get.v1;
-public sealed record PurchaseResponse(Guid? Id, Guid? SupplierId, DateTime? PurchaseDate, decimal TotalAmount, PurchaseStatus? Status, SupplierResponse? Supplier, ICollection<PurchaseItemResponse>? Items)
-{ 
-    public string? PurchaseDateFormatted => PurchaseDate?.ToString("MM/dd/yyyy"); 
-}
+public sealed record PurchaseResponse(Guid? Id, Guid? SupplierId, DateTime? PurchaseDate, decimal TotalAmount, PurchaseStatus? Status, SupplierResponse? Supplier, ICollection<PurchaseItemResponse>? Items);
