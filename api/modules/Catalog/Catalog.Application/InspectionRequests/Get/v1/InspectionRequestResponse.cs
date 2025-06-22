304c54919c51f5c587107945285921228dbd1f03
Roel D. Caperig <leor3824@yahoo.com>
2025-06-22 11:44:48 +0800

    Update Catalog and API documentation configurations
    - Added `FodyWeavers.xsd` to `.gitignore`.
    - Modified `AMIS.nswag` for runtime and Swagger URL.
    - Updated `InspectionRequestResponse` and `PurchaseResponse` records.
    - Enhanced `InspectionRequests.razor` UI with new columns.

--- api/modules/Catalog/Catalog.Application/InspectionRequests/Get/v1/InspectionRequestResponse.cs
+++ api/modules/Catalog/Catalog.Application/InspectionRequests/Get/v1/InspectionRequestResponse.cs
@@ -13,4 +13,3 @@ public sealed record InspectionRequestResponse(
     PurchaseResponse Purchase,
     EmployeeResponse Inspector
 );
-
--- api/modules/Catalog/Catalog.Application/Purchases/Get/v1/PurchaseResponse.cs
+++ api/modules/Catalog/Catalog.Application/Purchases/Get/v1/PurchaseResponse.cs
@@ -4,11 +4,11 @@ using AMIS.WebApi.Catalog.Domain.ValueObjects;
 
 namespace AMIS.WebApi.Catalog.Application.Purchases.Get.v1;
 public sealed record PurchaseResponse(
-    Guid? Id, 
-    Guid? SupplierId, 
-    DateTime? PurchaseDate, 
-    decimal TotalAmount, 
-    PurchaseStatus? Status, 
-    SupplierResponse? Supplier, 
+    Guid? Id,
+    Guid? SupplierId,
+    DateTime? PurchaseDate,
+    decimal TotalAmount,
+    PurchaseStatus? Status,
+    SupplierResponse? Supplier,
     ICollection<PurchaseItemResponse>? Items
     );
