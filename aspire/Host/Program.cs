336e058cf08064cef3711c4df2d7f7d5c188daec
Roel D. Caperig <leor3824@yahoo.com>
2026-01-19 22:20:31 +0800

    fix: Update database name from AMIS264 to AMIS266 in configuration files

--- aspire/Host/Program.cs
+++ aspire/Host/Program.cs
@@ -14,7 +14,7 @@ var password = builder.AddParameter("pg-password", "admin");
 
 var database = builder.AddPostgres("db", username, password, port: 5432)
     .WithDataVolume()
-    .AddDatabase("AMIS264");
+    .AddDatabase("AMIS266");
 
 var api = builder.AddProject<Projects.Server>("webapi")
     .WaitFor(database);
